447c3942d2533866d8438349add73e65e529b73a
Yuta <yuta.k@listenfield.com>
2024-07-12 08:14:43 +0900

    change program

--- GUI_app.py
+++ GUI_app.py
@@ -26,13 +26,6 @@ scenario_date_list = pd.read_csv('date_list.csv')
 scenario_date_list.Scenairo_date = pd.to_datetime(scenario_date_list.Scenario_date).dt.strftime('%Y-%m-%d')
 scenario_len = len(scenario_date_list) -1
 
-st.title('収穫時期予測アプリ')
-
-planting_date_calendar = st.date_input('播種日', planting_date_min, min_value=planting_date_min, max_value=planting_date_max)
-region_bar = st.selectbox('営農集団', region_group_list)
-scenario_date_bar = st.selectbox('シナリオ作成日', scenario_date_list.Scenairo_date, index=scenario_len)
-input_button = st.button('入力')
-
 def make_boxplot(file_name):
     df_boxplot = conn.read(file_name,input_format='csv',ttl=600)
     df_boxplot['Scenario_Date'] = pd.to_datetime(scenario_date_bar).strftime('%Y/%m/%d')
@@ -43,11 +36,22 @@ def make_boxplot(file_name):
     mean_date = pd.to_datetime(df_boxplot['MDAT'].mean(),format='%Y%j').strftime('%Y/%m/%d')
     return boxplot,quantile_25,quantile_75,mean_date
 
-if input_button:
+st.title('収穫時期予測アプリ(試験運用)')
+
+with st.container():
+    planting_date_calendar = st.date_input('播種日', planting_date_min, min_value=planting_date_min, max_value=planting_date_max)
+    region_bar = st.selectbox('営農集団', region_group_list)
+    scenario_date_bar = st.selectbox('シナリオ作成日', scenario_date_list.Scenairo_date, index=scenario_len)
+    input_button = st.button('入力')
+
+    if input_button:
     #region_en = region_list_en[region_list_jp.index(region_bar)]
-    region_en = [k for k, v in region_dict.items() if region_bar in v][0]
-    result_file = 'barley_storage/{0}/{2}/{0}_{1}.csv'.format(region_en, planting_date_calendar, scenario_date_bar)
-    fig,quantile_25,quantile_75,mean_date = make_boxplot(result_file)
-    st.plotly_chart(fig)
-    st.write('収穫日は{}から{}の予測です。'.format(quantile_25,quantile_75))
-    st.write('予測平均日は{}です。'.format(mean_date))
+        region_en = [k for k, v in region_dict.items() if region_bar in v][0]
+        result_file = 'barley_storage/{0}/{2}/{0}_{1}.csv'.format(region_en, planting_date_calendar, scenario_date_bar)
+        fig,quantile_25,quantile_75,mean_date = make_boxplot(result_file)
+        st.plotly_chart(fig)
+        st.write('収穫日は{}から{}の予測です。'.format(quantile_25,quantile_75))
+        st.write('予測平均日は{}です。'.format(mean_date))
+    st.divider()
+
+st.write('"提供"  [ListenField株式会社](https://www.listenfield.com/ja/home), [サッポロビール株式会社](https://www.sapporobeer.jp/)')
